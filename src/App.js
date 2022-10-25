1616c4c5b58fc01572ed6e2cdf35c351dd257954
hashmath3 <hashmathbasha@gmail.com>
2022-10-25 11:32:38 +0530

    topbar added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import Topbar from './scenes/global/Topbar';
+
+function app() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar/>
     </div>
   );
 }
 
-export default App;
+export default app;
