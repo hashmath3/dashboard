1ecad8bc536c264eef36b695e512b5c92f51d491
hashmath3 <hashmathbasha@gmail.com>
2022-10-26 11:13:23 +0530

    changes made at dashboard

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -16,7 +16,7 @@ import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutl
 import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
 import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
-
+import img1 from '../../images/hash.jpg'
 const Item = ({ title, to, icon, selected, setSelected }) => {
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -96,7 +96,7 @@ const Sidebar = () => {
                   alt="profile-user"
                   width="100px"
                   height="100px"
-                  src="../../assets/hash.jpg"
+                  src={img1}
                   style={{ cursor: "pointer", borderRadius: "50%" }}
                 />
               </Box>
